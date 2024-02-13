5a0c1fd6311c336fef18dc1181d7687c8ac98b01
orangech01 <chynara.sh.10@gmail.com>
2024-02-13 19:03:43 +0600

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,17 @@ const headerMenu = document.querySelector(".header-menu")
 const headerMenuBtn = document.querySelector(".header__menu-btn")
 
 headerMenuBtn.addEventListener('click', () => {
-  headerMenuBtn.classList.toggle('active')
-  headerMenu.classList.toggle('active')
-  body.classList.toggle('lock')
+    headerMenuBtn.classList.toggle('active')
+    headerMenu.classList.toggle('active')
+    body.classList.toggle('lock')
 })
 // burger menu -- ends
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 2, // Number of slides per view
+    slidesPerColumn: 2, // Number of slides per column
+
+});
