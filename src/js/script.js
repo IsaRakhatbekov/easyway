8b25cf95596acd0c2a74e98ccacd6022c5c66895
IsaRakhatbekov <isarahatbekov250@gmail.com>
2024-02-14 11:45:24 +0600

    some news

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,34 @@ headerMenuBtn.addEventListener('click', () => {
   body.classList.toggle('lock')
 })
 // burger menu -- ends
+
+
+
+// dropdown in form
+    const customSelect = document.getElementById('customSelect')
+    const selectedOption = document.getElementById('selectedOption')
+    const optionsList = document.getElementById('optionsList')
+    const countryCodeInput = document.getElementById('countryCodeInput')
+
+    let isOpen = false
+    selectedOption.addEventListener('click', function () {
+      isOpen = !isOpen
+      if (isOpen) {
+        optionsList.style.display = 'block'
+      } else {
+        optionsList.style.display = 'none'
+      }
+    })
+
+    optionsList.addEventListener('click', function (event) {
+      if (event.target.tagName === 'LI') {
+        const selectedValue = event.target.getAttribute('data-value')
+        selectedOption.textContent = event.target.textContent
+        countryCodeInput.placeholder = `Код страны(${selectedValue})`;
+        isOpen = false
+        optionsList.style.display = 'none'
+      }
+    })
+    
+
+// dropdown in form -- ends
